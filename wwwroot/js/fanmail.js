4c4f2ab8fb4d92a5e19bf45364856243c481d26a
Matthew Mecham <matthewmecham32@gmail.com>
2021-01-18 07:02:53 -0700

    Add project files.

---
+++ wwwroot/js/fanmail.js
@@ -0,0 +1,8 @@
+﻿var submit = document.getElementById("SubmitButton");
+
+submit.addEventListener("click", function (){
+    alert("From: " + document.getElementById("From").value + ", " +
+        "Email: " + document.getElementById("Email").value + ", " +
+        "Subject: " + document.getElementById("Subject").value + ", " +
+        "Message: " + document.getElementById("Message").value)
+});
